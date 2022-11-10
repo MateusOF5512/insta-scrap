2454ae5b8b234c6933d0509943f26c80b03e178f
M-Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2022-11-09 21:44:45 -0300

    Add files via upload

---
+++ variaveis/variaveis_css.py
@@ -0,0 +1,75 @@
+
+
+path_todos = "todos-tratado.csv"
+
+# - TOPO E RODAPÉ ------------------------------------------------------------------------
+html_title="""
+<h1 style="font-size:200%; color:#5B51D8; font-family:sans-serif;
+            text-align:center; ">INSTA STALKER BUSINESS</h1>
+"""
+html_rodape="""
+<hr style= "display: block;
+  margin-top: 0.5em;
+  margin-bottom: 0.5em;
+  margin-left: auto;
+  margin-right: auto;
+  border-style: inset;
+  border-width: 1.5px;">
+  <p style="color:Gainsboro; text-align: center;">Última atualização: 27/10/2022</p>
+"""
+
+
+
+# - PARTE 1 ------------------------------------------------------------------
+## 1.1 - Número de Doses & Vacinas Aplicadas:
+html_subheader_11="""
+<div class="card">
+  <div class="card-body" style="border-radius: 50px 50px 50px 50px; background: #5B51D8; 
+                                border:solid; border-color: black; border-width: 2px 2px 2px 2px;
+                                padding-top: 1px; padding-right: 25px; padding-bottom: 1px; padding-left: 25px; 
+                                width: 100%; height: 60px;">
+    <h3 class="card-title" style="background-color:#5B51D8; color:#F5F5F5; 
+                                  font-family:sans-serif; text-align: center; 
+                                  padding-top: 20px; padding-right: 1px; padding-bottom: 0px; padding-left: 1px;
+                                  font-size:150%;" 
+                                  >Monitoramento das Publicações</h3>
+  </div>
+"""
+
+html_header_10="""
+<div class="card">
+  <div class="card-body">
+    <h2 class="card-title" style="color:#5B51D8; font-family:sans-serif; 
+                                  text-align: center; padding: 10px 0; font-size:220%;"
+                                  >Monitoramento das Publicações</h2>
+  </div>
+</div>
+"""
+
+html_card_header_A1="""
+<div class="card">
+  <div class="card-body" style="border-radius: 15px 15px 0px 0px; background: #5B51D8;  
+                                border:solid; border-color: black; border-width: 1px 1px 1px 1px;
+                                padding-top: 1px; padding-right: 20px; padding-bottom: 1px; padding-left: 20px; 
+                                width: 100%; height: 40px;">
+    <h5 class="card-title" style="background-color:#5B51D8; color:#F5F5F5; font-family:sans-serif;
+                                  padding-top: 7px; padding-right: 1px; padding-bottom: 1px; padding-left: 1px; 
+                                  text-align: center;font-size: 120%;" 
+                                  >Últimas 12 Mídias Publicadas</h5>
+  </div>
+</div>
+"""
+
+html_card_header_A2="""
+<div class="card">
+  <div class="card-body" style="border-radius: 15px 15px 0px 0px; background: #5B51D8;  
+                                border:solid; border-color: black; border-width: 1px 1px 1px 1px;
+                                padding-top: 1px; padding-right: 20px; padding-bottom: 1px; padding-left: 20px; 
+                                width: 100%; height: 40px;">
+    <h5 class="card-title" style="background-color:#5B51D8; color:#F5F5F5; font-family:sans-serif;
+                                  padding-top: 7px; padding-right: 1px; padding-bottom: 1px; padding-left: 1px; 
+                                  text-align: center;font-size: 120%;" 
+                                  >Últimos 12 Vídeos Publicados</h5>
+  </div>
+</div>
+"""
