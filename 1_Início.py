45aca20cc91997db67101c63587a69dbf14fe41e
M-Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2022-11-09 21:43:26 -0300

    Add files via upload

--- 1_Início.py
+++ 1_Início.py
@@ -4,57 +4,36 @@ import streamlit as st
 from PIL import Image
 from io import BytesIO
 
+from variaveis.variaveis_css import *
+
 import pandas as pd
 import numpy as np
 import requests
 
 from plots.plots_insta1 import *
+from plots.layout import *
 
 im = Image.open("instagram.png")
 st.set_page_config(page_title="Instagram Monitor", page_icon=im, layout="wide")
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 
+topo()
+st.markdown("""---""")
 
-st.title("INSTA SCRAPIZ")
+perfil = st.text_input("Insira o nome da conta que deseja pesquisar:")
+
+
+if len(perfil) != 0:
+    parte1(perfil)
+
+
 st.markdown("""---""")
 
 
 
-col1A, col2A = st.columns([1, 10])
-with col1A:
-    st.text("")
-    st.text("")
-    bot = st.button("Pesquisar")
-with col2A:
-    perfil = st.text_input("Insira o nome da conta que deseja pesquisar:", value="instagram")
-
-st.markdown("""---""")
-
-
-
-if bot:
-    res = requi(perfil)
-    df = convert_csv1(res)
-
-    df_midia = convert_csv_midia(res)
-    df_video = convert_csv_video(res)
-
-    response = requests.get(df["foto"][0])
-    img = Image.open(BytesIO(response.content))
-
-    col1A, col2A = st.columns([10, 5])
-    with col1A:
-        st.text("")
-        st.text("")
-        st.text("")
-        st.text("")
-        st.dataframe(df)
-    with col2A:
-        st.image(img, use_column_width=True)
-
-    st.dataframe(df_midia)
-    st.dataframe(df_video)
 
 
 
 
+
+rodape()
